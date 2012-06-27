04c0a0547ccab5ac754ab43d63267922003e1c26
justinvdm <jvandermerwe@cs.uct.ac.za>
2012-06-27 17:10:23 +0200

    removed unnecessary tasseo code

--- static/diamondash.js
+++ static/diamondash.js
@@ -1,6 +1,5 @@
 var graphs = []; // rickshaw objects
-var datum = []; // metric data
-var aliases = []; // alias strings
+var data = []; // metric data
 
 // minutes of data in the live feed
 var period = (typeof period == 'undefined') ? 5 : period;
@@ -9,14 +8,13 @@ function constructWidgets() {
 	graphElements = document.querySelectorAll('.graph'); 
 
     for (var i = 0; i < graphElements.length; i++) {
-        aliases[i] = 'not important';
-		datum[i] = [{ x:0, y:0 }];
+		data[i] = [{ x:0, y:0 }];
 		graphs[i] = new Rickshaw.Graph({
 			element: graphElements[i],
 			interpolation: 'step-after',
 			series: [{
 			color: '#afdab1',
-			data: datum[i]
+			data: data[i]
 			}]
 		});
 		graphs[i].render();
@@ -35,44 +33,27 @@ function constructUrl(period) {
 		targets += ('target=' + encodeURI($.trim(widgetElements[i].id)));
 	}
 	currentUrl = '/render/?' + targets + '&from=-' + period + 'minutes&format=json';
-
-	console.log(currentUrl);
 }
 
 // refresh the graph
-function refreshData(immediately) {
-
+function updateWidgets() {
 	getData(function(values) {
-		for (var i=0; i<graphs.length; i++) {
-			for (var j=0; j<values[i].length; j++) {
+		for (var i = 0; i < graphs.length; i++) {
+			for (var j = 0; j < values[i].length; j++) {
 				if (typeof values[i][j] !== "undefined") {
-					datum[i][j] = values[i][j];
+					data[i][j] = values[i][j];
 				}
 			}
-
-			// we want to render immediately, i.e.
-			// as soon as ajax completes
-			// used for time period / pause view
-			if (immediately) {
-				updateGraphs(i);
-			}
+			updateGraph(i);
 		}
 
 		values = null;
 	});
-
-	// we can wait until all data is gathered, i.e.
-	// the live refresh should happen synchronously
-	if (!immediately) {
-		for (var i=0; i<graphs.length; i++) {
-			updateGraphs(i);
-		}
-	}
 }
 
 // retrieve the data from Graphite
-function getData(dataCallback) {
-	var obtainedDatum = [];
+function getData(cbDataReceived) {
+	var obtainedData = [];
 	$.ajax({
 
 		/*beforeSend: function(xhr) {
@@ -90,16 +71,16 @@ function getData(dataCallback) {
 		url: currentUrl
 	}).done(function(d) {
 		if (d.length > 0) {
-			for (var i=0; i<d.length; i++) {
-				obtainedDatum[i] = [];
-				obtainedDatum[i][0] = {
+			for (var i = 0; i < d.length; i++) {
+				obtainedData[i] = [];
+				obtainedData[i][0] = {
 					x: d[i].datapoints[0][1],
-	y: d[i].datapoints[0][0] || graphs[i].lastKnownValue || 0
+					y: d[i].datapoints[0][0] || graphs[i].lastKnownValue || 0
 				};
-				for (var j=1; j<d[i].datapoints.length; j++) {
-					obtainedDatum[i][j] = {
+				for (var j = 1; j < d[i].datapoints.length; j++) {
+					obtainedData[i][j] = {
 						x: d[i].datapoints[j][1],
-	y: d[i].datapoints[j][0] || graphs[i].lastKnownValue
+						y: d[i].datapoints[j][0] || graphs[i].lastKnownValue
 					};
 					if (typeof d[i].datapoints[j][0] === "number") {
 						graphs[i].lastKnownValue = d[i].datapoints[j][0];
@@ -107,17 +88,17 @@ function getData(dataCallback) {
 				}
 			} 
 		}
-		dataCallback(obtainedDatum);
+		cbDataReceived(obtainedData);
 	});
 }
 
 // perform the actual graph object and
 // overlay name and number updates
-function updateGraphs(i) {
+function updateGraph(i) {
 	// update our graph
 	graphs[i].update();
-	if (datum[i][datum[i].length - 1] !== undefined) {
-		var lastValue = datum[i][datum[i].length - 1].y;
+	if (data[i][data[i].length - 1] !== undefined) {
+		var lastValue = data[i][data[i].length - 1].y;
 		var lastValueDisplay;
 		if ((typeof lastValue == 'number') && lastValue < 2.0) {
 			lastValueDisplay = Math.round(lastValue*1000)/1000;
@@ -127,14 +108,11 @@ function updateGraphs(i) {
 	}
 }
 
-// build our graph objects
 constructWidgets();
-
-// build our url
 constructUrl(period);
 
-refreshData("now");
+updateWidgets(true);
 
 // define our refresh and start interval
-var refreshInterval = (typeof refresh == 'undefined') ? 2000 : refresh;
-var refreshId = setInterval(refreshData, refreshInterval);
+var updateInterval = (typeof refresh == 'undefined') ? 2000 : refresh;
+var updateId = setInterval(updateWidgets, updateInterval);
