c85662ecb43644a92e73421e15c7adcdb3f1fc6f
justinvdm <jvandermerwe@cs.uct.ac.za>
2012-07-05 03:00:18 +0200

    removed a test line I forgot to take out

--- diamondash/static/diamondash.js
+++ diamondash/static/diamondash.js
@@ -56,7 +56,6 @@ function updateWidgets() {
 			for (var metricName in results) {
 				var resultMetricData = results[metricName];
 				var graphMetricData = graph.data[metricName];
-				graphiteMetricData = resultMetricData
 
 				for (var j = 0; j < resultMetricData.length; j++) {
 					graphMetricData[j] = resultMetricData[j];
