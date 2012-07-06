552863f8872574919e2489408c3c06a50bb2cc5e
justinvdm <jvandermerwe@cs.uct.ac.za>
2012-07-06 02:40:20 +0200

    improved the graph displays

--- diamondash/static/diamondash.js
+++ diamondash/static/diamondash.js
@@ -1,44 +1,114 @@
 var DEFAULT_REQUEST_INTERVAL = 2000
-var DEFAULT_GRAPH_COLOUR = '#0051cc'
+var DEFAULT_GRAPH_COLOUR = '#3333cc'
 
 var graphs = [];
 var requestInterval = config.requestInterval || DEFAULT_REQUEST_INTERVAL;
 
+function Hover(graphObject, widgetMetricsConfig, legend) {
+	var HoverDetail = Rickshaw.Class.create(Rickshaw.Graph.HoverDetail, {
+		render: function(args) {
+			var timeLabel = legend.querySelector('.graph-time-label');
+			timeLabel.innerHTML = args.formattedXValue;
+
+			// for each metric
+			args.detail.sort(function(a, b) { return a.order - b.order }).forEach(function(d) {
+				var title = widgetMetricsConfig[d.name].title;
+				var key = legend.querySelector('#metric-key-container-' + d.name);
+				var label = key.querySelector('#metric-key-label-' + d.name);
+				label.innerHTML = title + ": " + d.formattedYValue;
+
+				var dot = document.createElement('div');
+				dot.className = 'dot';
+				dot.style.top = graphObject.y(d.value.y0 + d.value.y) + 'px';
+				dot.style.borderColor = d.series.color;
+
+				this.element.appendChild(dot);
+				dot.className = 'dot active';
+
+				this.show()
+			}, this);
+		}
+	});
+
+	return new HoverDetail({graph: graphObject});
+}
+
+function constructMetricKey(legend, metricName, metricTitle, metricColor) {
+	var metricKey = document.createElement('div');
+	metricKey.id = 'metric-key-container-'+metricName;
+	metricKey.className = 'metric-key-container';
+
+	var swatch = document.createElement('div');
+	swatch.className = 'metric-key-swatch';
+	swatch.id = 'metric-key-swatch-'+metricName;
+	swatch.style.backgroundColor = metricColor;
+
+	var label = document.createElement('div');
+	label.className = 'metric-key-label';
+	label.id = 'metric-key-label-'+metricName;
+	label.innerHTML = metricTitle;
+
+	metricKey.appendChild(swatch);
+	metricKey.appendChild(label);
+	legend.appendChild(metricKey);
+}
+
 // construct the widget objects using the config
 function constructWidgets() {
-	var graphElements = document.querySelectorAll('.graph'); 
-    for (var i = 0; i < graphElements.length; i++) {
-		var widgetName = $.trim(graphElements[i].id);
+	var graphWidgets = document.querySelectorAll('.graph-widget'); 
+    for (var i = 0; i < graphWidgets.length; i++) {
+		var widgetElement = graphWidgets[i];
+		var legend = widgetElement.querySelector('.legend');
+		var widgetName = $.trim(widgetElement.id);
 		var widgetConfig = config.widgets[widgetName];
+		var graphObject = null;
+		var graphData = {};
 
-		graphs[i] = {
-			name: widgetName,
-			data: {},
-			object: undefined
-		};
-
+		// build the structures needed for each metric
 		var j = 0;
 		var metricSeries = [];
 		for (var metricName in widgetConfig.metrics) {
 			var metric = widgetConfig.metrics[metricName];
 
-			graphs[i].data[metricName] = [{ x:0, y:0 }];
+			graphData[metricName] = [{ x:0, y:0 }];
 			var metricColor = metric.color || DEFAULT_GRAPH_COLOUR;
 			metricSeries[j] = {
-				data: graphs[i].data[metricName],
-				color: metricColor
+				data: graphData[metricName],
+				color: metricColor,
+				name: metricName
 			}
+
+			constructMetricKey(legend, metricName, metric.title, metricColor);
 
 			j++;
 		}
-		
-		graphs[i].object = new Rickshaw.Graph({
-			element: graphElements[i],
+
+		graphObject = new Rickshaw.Graph({
+			element: widgetElement.querySelector('.graph'),
 			renderer: 'line',
 			series: metricSeries
 		});
+		
+		var xAxis = new Rickshaw.Graph.Axis.Time({
+			graph: graphObject 
+		});
 
-		graphs[i].object.render();
+		var yAxis = new Rickshaw.Graph.Axis.Y({
+			element: widgetElement.querySelector('.y-axis'),
+			orientation: 'left',
+			graph: graphObject,
+			tickFormat: Rickshaw.Fixtures.Number.formatKMBT,
+		});
+
+		var hover = new Hover(graphObject, widgetConfig.metrics, legend);
+
+		graphObject.render();
+
+		graphs[i] = {
+			name: widgetName,
+			data: graphData,
+			object: graphObject
+		};
 	}
 }
 
