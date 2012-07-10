0d7232a29f514ce6b7c3d8703f66ffde0666f5fb
justinvdm <jvandermerwe@cs.uct.ac.za>
2012-07-10 03:06:25 +0200

    got last times and values showing by default, and times and values at the cursor position showing on hover (in bold)

--- diamondash/static/diamondash.js
+++ diamondash/static/diamondash.js
@@ -1,53 +1,71 @@
 var DEFAULT_REQUEST_INTERVAL = 2000
 var DEFAULT_GRAPH_COLOUR = '#3333cc'
-var TIME_HOVER_OFFSET_X = 160;
-var TIME_HOVER_OFFSET_Y = -22;
 
 var graphs = [];
 var requestInterval = config.requestInterval || DEFAULT_REQUEST_INTERVAL;
 
-function Hover(graphObject, widgetMetricsConfig, legend, timeLabel) {
+function Hover(graph, widgetMetricsConfig, legend, hoverLegend) {
 	var HoverDetail = Rickshaw.Class.create(Rickshaw.Graph.HoverDetail, {
 		render: function(args) {
-			hoverX = args.detail[0].graphX;
-			timeLabel.innerHTML = args.formattedXValue;
-			timeLabelPosition = hoverX + TIME_HOVER_OFFSET_X;
-			timeLabel.style.left = timeLabelPosition + 'px';
+			var hoverTimeLabel = hoverLegend.querySelector('.graph-time-label');
+			hoverTimeLabel.innerHTML = args.formattedXValue;
 
 			// for each metric
 			args.detail.sort(function(a, b) { return a.order - b.order }).forEach(function(d) {
 				var title = widgetMetricsConfig[d.name].title;
-				var key = legend.querySelector('#metric-key-container-' + d.name);
+				var key = hoverLegend.querySelector('#metric-key-container-' + d.name);
 				var label = key.querySelector('#metric-key-label-' + d.name);
 				var valueLabel = key.querySelector('#metric-key-value-label-' + d.name);
 
 				label.innerHTML = title + ": ";
 				valueLabel.innerHTML = d.formattedYValue;
-				valueLabel.className = 'metric-key-value-label active';
 
 				var dot = document.createElement('div');
 				dot.className = 'dot';
-				dot.style.top = graphObject.y(d.value.y0 + d.value.y) + 'px';
+				dot.style.top = graph.object.y(d.value.y0 + d.value.y) + 'px';
 				dot.style.borderColor = d.series.color;
 				this.element.appendChild(dot);
 				dot.className = 'dot active';
 
 				this.show()
 			}, this);
+		},
+
+		hide: function() {
+			this.visible = false;
+			this.element.classList.add('inactive');
+			legend.classList.remove('inactive');
+			hoverLegend.classList.add('inactive');
+
+			if (typeof this.onHide == 'function') {
+				this.onHide();
+			}
+		},
+
+		show: function() {
+			this.visible = true;
+			this.element.classList.remove('inactive');
+			legend.classList.add('inactive');
+			hoverLegend.classList.remove('inactive');
+
+			if (typeof this.onShow == 'function') {
+				this.onShow();
+			}
 		}
 	});
 
-	return new HoverDetail({graph: graphObject});
+	return new HoverDetail({graph: graph.object});
 }
 
+// Construct the passed in metric's key in the legend
 function constructMetricKey(legend, metricName, metricTitle, metricColor) {
 	var metricKey = document.createElement('div');
-	metricKey.id = 'metric-key-container-'+metricName;
+	metricKey.id = 'metric-key-container-' + metricName;
 	metricKey.className = 'metric-key-container';
 
 	var swatch = document.createElement('div');
 	swatch.className = 'metric-key-swatch';
-	swatch.id = 'metric-key-swatch-'+metricName;
+	swatch.id = 'metric-key-swatch-' + metricName;
 	swatch.style.backgroundColor = metricColor;
 
 	var label = document.createElement('div');
@@ -57,7 +75,7 @@ function constructMetricKey(legend, metricName, metricTitle, metricColor) {
 
 	var valueLabel = document.createElement('span');
 	valueLabel.className = 'metric-key-value-label';
-	valueLabel.id = 'metric-key-value-label-'+metricName;
+	valueLabel.id = 'metric-key-value-label-' + metricName;
 	valueLabel.innerHTML = ' ';
 
 	metricKey.appendChild(swatch);
@@ -73,7 +91,7 @@ function constructWidgets() {
 		var widgetElement = graphWidgets[i];
 		var graphElement = widgetElement.querySelector('.graph');
 		var legend = widgetElement.querySelector('.legend');
-		var timeLabel = widgetElement.querySelector('.graph-time-label');
+		var hoverLegend = widgetElement.querySelector('.hover-legend');
 		var widgetName = $.trim(widgetElement.id);
 		var widgetConfig = config.widgets[widgetName];
 		var graphObject = null;
@@ -94,6 +112,7 @@ function constructWidgets() {
 			}
 
 			constructMetricKey(legend, metricName, metric.title, metricColor);
+			constructMetricKey(hoverLegend, metricName, metric.title, metricColor);
 
 			j++;
 		}
@@ -115,17 +134,16 @@ function constructWidgets() {
 			tickFormat: Rickshaw.Fixtures.Number.formatKMBT,
 		});
 
-		var hover = new Hover(graphObject, widgetConfig.metrics, legend, timeLabel);
-		timeLabel.style.marginTop = graphElement.offsetHeight + TIME_HOVER_OFFSET_Y;
-
 		graphObject.render();
 
 		graphs[i] = {
 			name: widgetName,
 			data: graphData,
 			object: graphObject,
-			container: widgetElement
+			containerElement: widgetElement
 		};
+
+		var hover = new Hover(graphs[i], widgetConfig.metrics, legend, hoverLegend);
 	}
 }
 
@@ -134,20 +152,31 @@ function constructUrl(widgetName) {
 	return '/render/' + config.dashboardName + '/' + widgetName;
 }
 
+// formats an time value on the x axis into a UTC string
+function XFormatter(x) { 
+	return new Date(x * 1000).toUTCString(); 
+}
+
 var YFormatter = Rickshaw.Fixtures.Number.formatKMBT;
 function updateLegendValues(graph) {
-	var graphWidget = graph.container; 
+	var graphWidget = graph.containerElement; 
+	var legend = graphWidget.querySelector('.legend');
+	var timeLabel = legend.querySelector('.graph-time-label');
 
+	var lastX = -1;
 	var metrics = config.widgets[graph.name].metrics;
 	for (metricName in metrics) {
-		var valueLabel = graphWidget.querySelector(
-			'#metric-key-value-label-' + metricName);
-		var metricData = graph.data[metricName]
+		var valueLabel = legend.querySelector('#metric-key-value-label-' + metricName);
+		var metricData = graph.data[metricName];
 		var lastCoord = metricData[metricData.length-1];
 		var lastValue = YFormatter(lastCoord.y);
+		if (lastCoord.x > lastX)
+			lastX = lastCoord.x;
 		valueLabel.innerHTML = lastValue;
 		valueLabel.className = 'metric-key-value-label inactive';
 	}
+
+	timeLabel.innerHTML = XFormatter(lastX);
 }
 
 // called each update interval
@@ -167,7 +196,7 @@ function updateWidgets() {
 
 			graph.object.update();
 
-			//updateLegendValues(graph);
+			updateLegendValues(graph);
 		});
 	});
 }
