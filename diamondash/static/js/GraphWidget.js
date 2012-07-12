3be98f310d9eb87ade27f06bb1f1b2390ff62939
justinvdm <jvandermerwe@cs.uct.ac.za>
2012-07-12 13:23:05 +0200

    fixed the legend key colors to change with the graph's warning color change

--- diamondash/static/js/GraphWidget.js
+++ diamondash/static/js/GraphWidget.js
@@ -122,6 +122,10 @@ GraphWidget.prototype.updateGraphDetails = function() {
 			var metric = metrics[metricName];
 			var valueLabel = this.legend.querySelector(
 				'#metric-key-value-label-' + metricName);
+			var swatch = this.legend.querySelector(
+				'#metric-key-swatch-' + metricName);
+			var hoverSwatch = this.hoverLegend.querySelector(
+				'#metric-key-swatch-' + metricName);
 			var metricData = this.getMetricData(metricName);
 			var lastCoord = metricData[metricData.length-1];
 			var lastValue = yFormatter(lastCoord.y);
@@ -136,9 +140,13 @@ GraphWidget.prototype.updateGraphDetails = function() {
 				var warning_color = metric.warning_color;
 				this.setMetricColor(metricName, warning_color);
 				valueLabel.style.color = warning_color;
+				swatch.style.backgroundColor = warning_color;
+				hoverSwatch.style.backgroundColor = warning_color;
 			} else {
 				this.setMetricColor(metricName, metric.color);
 				valueLabel.style.color = '';
+				swatch.style.backgroundColor = metric.color;
+				hoverSwatch.style.backgroundColor = metric.color;
 			}
 		}
 	}
