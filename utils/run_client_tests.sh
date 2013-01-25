939bdbcd54f9be0647ef300f0b1a349261ce8e95
Jeremy Thurgood <firxen@gmail.com>
2013-01-25 09:29:16 +0200

    Support BSD find in test script.

--- utils/run_client_tests.sh
+++ utils/run_client_tests.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 cd "diamondash"
-CLIENT_TESTS=`find -name "*.test.js" | while read f; do echo "${f%.js}"; done`
+CLIENT_TESTS=`find . -name "*.test.js" | while read f; do echo "${f%.js}"; done`
 
 export NODE_ENV=test 
 export NODE_PATH=$NODE_PATH:"`pwd`/widgets":"`pwd`/public/js/"
