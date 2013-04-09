b123deeb99f2d9df9c57060b82db815bb1d7b124
justinvdm <justinvdm@lavabit.com>
2013-04-09 18:05:39 +0200

    http_request utlity method (*missions*)

--- diamondash/tests/test_utils.py
+++ diamondash/tests/test_utils.py
@@ -1,6 +1,12 @@
+import json
+from urllib import urlencode
+
 from twisted.trial import unittest
+from twisted.internet.defer import inlineCallbacks
+from twisted.web.error import Error as WebError
 
 from diamondash import utils
+from diamondash.tests.utils import MockHttpServer
 
 
 class UtilsTestCase(unittest.TestCase):
@@ -86,3 +92,74 @@ class UtilsTestCase(unittest.TestCase):
         self.assertEqual(utils.floor_time(15, 10), 10)
         self.assertEqual(utils.floor_time(18, 10), 10)
         self.assertEqual(utils.floor_time(22, 10), 20)
+
+
+class HttpUtilsTestCase(unittest.TestCase):
+    def setUp(self):
+        self.response_body = ""
+        self.response_code = 200
+        self.response_headers = {}
+        self.server = MockHttpServer(handler=self.handle_request)
+        return self.server.start()
+
+    def tearDown(self):
+        return self.server.stop()
+
+    def handle_request(self, request):
+        request.setResponseCode(self.response_code)
+        for h_name, h_values in self.response_headers.iteritems():
+            request.responseHeaders.setRawHeaders(h_name, h_values)
+        self.server.queue.put(request)
+        return self.response_body
+
+    def assert_http_request_response(self, body, code=200, headers={}):
+        self.response_body = body
+        self.response_code = code
+        self.response_headers = headers
+        d = utils.http_request(self.server.url)
+
+        def assert_response(response):
+            self.assertEqual(response['body'], body)
+            self.assertEqual(response['status'], str(code))
+            for k, v in headers.iteritems():
+                self.assertEqual(response['headers'][k], v)
+        d.addCallback(assert_response)
+        d.addErrback(lambda failure: failure.trap(WebError))
+        return d
+
+    @inlineCallbacks
+    def test_http_request_response_handling(self):
+        yield self.assert_http_request_response("")
+        yield self.assert_http_request_response(json.dumps({'a': [1, 2]}))
+        yield self.assert_http_request_response("Not Found", code=404)
+        yield self.assert_http_request_response(
+            "Internal Server Error", code=500, headers={'a': ['b']})
+
+    @inlineCallbacks
+    def assert_last_request(self, args, data="", headers={}, method='GET'):
+        request = yield self.server.queue.get()
+        self.assertEqual(request.method, method)
+        self.assertEqual(request.args, args)
+
+        for k, v in headers.iteritems():
+            self.assertEqual(request.requestHeaders.getRawHeaders(k), v)
+
+    def test_http_request_for_GET(self):
+        utils.http_request(
+            "%s?%s" % (self.server.url, urlencode({'a': 'lerp', 'b': 'larp'})),
+            headers={'Han': 'Solo', 'Mon': 'Mothma'},
+            method='GET')
+        return self.assert_last_request(
+            args={'a': ['lerp'], 'b': ['larp']},
+            headers={'Han': ['Solo'], 'Mon': ['Mothma']},
+            method='GET')
+
+    def test_http_request_for_POST(self):
+        data = json.dumps({'a': 'lerp', 'b': 'larp'})
+        utils.http_request(self.server.url, data=data, method='POST')
+        return self.assert_last_request(args={}, data=data, method='POST')
+
+    def test_http_request_for_DELETE(self):
+        data = json.dumps({'a': 'lerp', 'b': 'larp'})
+        utils.http_request(self.server.url, data=data, method='DELETE')
+        return self.assert_last_request(args={}, data=data, method='DELETE')
