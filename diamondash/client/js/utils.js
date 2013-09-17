59f5546e05cdc7ba0401ac2c36e421f556dbf9bb
justinvdm <justinvdm.0.1.0@gmail.com>
2013-09-17 16:21:36 +0200

    Add objectByName and bindEvents utility methods

---
+++ diamondash/client/js/utils.js
@@ -0,0 +1,25 @@
+diamondash.utils = function() {
+  function objectByName(name, that) {
+    return _(name.split( '.' )).reduce(
+      function(obj, propName) { return obj[propName]; },
+      that || this);
+  }
+
+  function bindEvents(events, that) {
+    that = that || this;
+
+    _(events).each(function(fn, e) {
+      var parts = e.split(' '),
+          event = parts[0],
+          entity = parts[1];
+
+      if (entity) { that.listenTo(objectByName(entity, that), event, fn); }
+      else { that.on(event, fn); }
+    });
+  }
+
+  return {
+    objectByName: objectByName,
+    bindEvents: bindEvents
+  };
+}.call(this);
---
+++ diamondash/client/tests/utils.test.js
@@ -0,0 +1,45 @@
+describe.only("diamondash.utils", function() {
+  var utils = diamondash.utils;
+
+  describe(".objectByName()", function() {
+    it("should get an object defined directly on the context object",
+    function() {
+      assert.equal(
+        utils.objectByName('thing', {thing: 3}),
+        3);
+    });
+
+    it("should get an object defined on a property of the context object",
+    function() {
+      assert.equal(
+        utils.objectByName('thing.subthing', {thing: {subthing: 23}}),
+        23);
+    });
+  });
+
+  describe(".bindEvents()", function() {
+    var thing;
+
+    beforeEach(function() {
+      thing = Object.create(Backbone.Events);
+    });
+
+    it("should bind events defined on the object itself", function(done) {
+      utils.bindEvents({
+        'fire': function() { done(); }
+      }, thing);
+
+      thing.trigger('fire');
+    });
+
+    it("should bind events defined on nested objects", function(done) {
+      thing.subthing = {subsubthing: Object.create(Backbone.Events)};
+
+      utils.bindEvents({
+        'fire subthing.subsubthing': function() { done(); }
+      }, thing);
+
+      thing.subthing.subsubthing.trigger('fire');
+    });
+  });
+});
