2b7f75fd06c54f0216fb89bc0e9d72f08addf22d
justinvdm <justinvdm.0.1.0@gmail.com>
2013-09-17 09:19:48 +0200

    Get tests to run with karma

--- diamondash/client/tests/init.js
+++ diamondash/client/tests/init.js
@@ -1,16 +1,1 @@
-assert = require('assert');
-sinon = require('sinon');
-
-
-document = require("jsdom").jsdom("<html><head></head><body></body></html>");
-window = document.createWindow();
-navigator = window.navigator;
-
-$ = require('jquery');
-_ = require('../vendor/underscore');
-Backbone = require('../vendor/backbone');
-Backbone.$ = $;
-
-window = global;
-require('../../public/js/vendor');
-require('../../public/js/diamondash');
+window.assert = chai.assert;
