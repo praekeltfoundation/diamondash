ace18b623df0b20fa423c0d1fde576bf98d8f515
justinvdm <justinvdm.0.1.0@gmail.com>
2013-10-07 16:07:32 +0200

    Refactor GraphWidgetConfig out of GraphWidget

--- diamondash/widgets/graph/__init__.py
+++ diamondash/widgets/graph/__init__.py
@@ -1,3 +1,3 @@
-__all__ = ['GraphWidget']
+from diamondash.widgets.graph.graph import GraphWidgetConfig, GraphWidget
 
-from diamondash.widgets.graph.graph import GraphWidget
+__all__ = ['GraphWidgetConfig', 'GraphWidget']
