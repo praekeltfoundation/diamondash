a1291adc36ab366ceecebfda2a40921daecece76
justinvdm <justinvdm.0.1.0@gmail.com>
2013-10-22 20:43:21 +0200

    Add title attributes to the widgets in the DashboardModel fixtures

--- diamondash/client/tests/fixtures/dashboard.simple.fixture.js
+++ diamondash/client/tests/fixtures/dashboard.simple.fixture.js
@@ -2,10 +2,12 @@ diamondash.test.fixtures.add('diamondash.dashboard.DashboardModel:simple', {
   name: 'dashboard-1',
   poll_interval: 50,
   widgets: [{
+    title: 'Widget 1',
     name: 'widget-1',
     type_name: 'toy',
     stuff: 'foo'
   }, {
+    title: 'Widget 2',
     name: 'widget-2',
     type_name: 'toy',
     stuff: 'bar'
