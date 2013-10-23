5abf046dea598affa6087809b4c78b3809bb7179
justinvdm <justinvdm.0.1.0@gmail.com>
2013-10-23 13:28:13 +0200

    Add a global diamondash config

--- diamondash/client/js/index.js
+++ diamondash/client/js/index.js
@@ -1,4 +1,13 @@
 window.diamondash = function() {
+  var DiamondashConfigModel = Backbone.RelationalModel.extend({
+    defaults: {
+      url_prefix: ''
+    }
+  });
+
+  var config = new DiamondashConfigModel();
+
   return {
+    config: config
   };
 }.call(this);
