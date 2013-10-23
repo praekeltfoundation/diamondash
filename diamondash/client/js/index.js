519b01fa96a5997376bd3f79e40c0b8ca25a273f
justinvdm <justinvdm.0.1.0@gmail.com>
2013-10-23 13:29:16 +0200

    Add diamondash.url() for constructing diamondash urls

--- diamondash/client/js/index.js
+++ diamondash/client/js/index.js
@@ -7,7 +7,15 @@ window.diamondash = function() {
 
   var config = new DiamondashConfigModel();
 
+  function url() {
+    var parts = _(arguments).toArray();
+    parts.unshift(config.get('url_prefix'));
+    parts.unshift('/');
+    return diamondash.utils.joinPaths.apply(this, parts);
+  }
+
   return {
+    url: url,
     config: config
   };
 }.call(this);
