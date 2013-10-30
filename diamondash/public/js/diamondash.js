0ed2f041278e522c665450a9792bff88522716b6
justinvdm <justinvdm.0.1.0@gmail.com>
2013-10-30 12:51:39 +0200

    Rebuild diamondash.js after allowing optional auth to be used

--- diamondash/public/js/diamondash.js
+++ diamondash/public/js/diamondash.js
@@ -1,21 +1,9 @@
 window.diamondash = function() {
-  var AuthModel = Backbone.RelationalModel.extend({
-    defaults: {
-      all: false
-    },
-
-    stringify: function() {
-      return diamondash.utils.basicAuth(
-        this.get('username'),
-        this.get('password'));
-    }
-  });
-
   var DiamondashConfigModel = Backbone.RelationalModel.extend({
     relations: [{
       type: Backbone.HasOne,
       key: 'auth',
-      relatedModel: AuthModel,
+      relatedModel: 'diamondash.models.AuthModel',
       includeInJSON: false
     }],
 
@@ -24,18 +12,16 @@ window.diamondash = function() {
     }
   });
 
-  var config = new DiamondashConfigModel();
-
   function url() {
     var parts = _(arguments).toArray();
-    parts.unshift(config.get('url_prefix'));
+    parts.unshift(diamondash.config.get('url_prefix'));
     parts.unshift('/');
     return diamondash.utils.joinPaths.apply(this, parts);
   }
 
   return {
     url: url,
-    config: config
+    DiamondashConfigModel: DiamondashConfigModel
   };
 }.call(this);
 
@@ -459,6 +445,41 @@ diamondash.components.charts = function() {
     AxisView: AxisView,
     ChartView: ChartView,
     Dimensions: Dimensions
+  };
+}.call(this);
+
+diamondash.models = function() {
+  var AuthModel = Backbone.RelationalModel.extend({
+    defaults: {
+      all: false
+    },
+
+    stringify: function() {
+      return diamondash.utils.basicAuth(
+        this.get('username'),
+        this.get('password'));
+    }
+  });
+
+  var Model = Backbone.RelationalModel.extend({
+    sync: function(method, model, options) {
+      options = options || {};
+
+      if (options.auth || diamondash.config.get('auth').get('all')) {
+        options.beforeSend = function(xhr) {
+          xhr.setRequestHeader(
+            'Authorization',
+            diamondash.config.get('auth').stringify());
+        };
+      }
+
+      return Backbone.sync.call(this, method, model, options);
+    }
+  });
+
+  return {
+    Model: Model,
+    AuthModel: AuthModel
   };
 }.call(this);
 
@@ -1266,3 +1287,7 @@ diamondash.dashboard = function() {
     DashboardRowModel: DashboardRowModel
   };
 }.call(this);
+
+(function() {
+  diamondash.config = new diamondash.DiamondashConfigModel();
+}).call(this);
