330699861f684fccc0d40ede098b805e7706f55b
justinvdm <justinvdm.0.1.0@gmail.com>
2013-11-28 11:06:48 +0200

    Bump version (0.3.0 -> 0.3.1)

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ install_requires, dependency_links = parse_requirements(
 
 setup(
     name="diamondash",
-    version="0.3.0",
+    version="0.3.1",
     url='https://github.com/praekelt/diamondash',
     license='BSD',
     description="A dashboard frontend for Graphite",
