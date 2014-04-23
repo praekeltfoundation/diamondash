8a22a7b72186cc465e7c3d4ffe7de704496cd9ae
justinvdm <justinvdm.0.1.0@gmail.com>
2014-04-23 15:58:31 +0200

    Rebuild

--- diamondash/public/js/diamondash.js
+++ diamondash/public/js/diamondash.js
@@ -1240,6 +1240,120 @@ diamondash.widgets.graph.views = function() {
   };
 }.call(this);
 
+diamondash.widgets.pie = function() {
+  return {
+  };
+}.call(this);
+
+diamondash.widgets.pie.models = function() {
+  var widgets = diamondash.widgets,
+      chart = diamondash.widgets.chart;
+
+  var PieModel = chart.models.ChartModel.extend({
+  });
+
+  widgets.registry.models.add('pie', PieModel);
+
+  return {
+    PieModel: PieModel
+  };
+}.call(this);
+
+diamondash.widgets.pie.views = function() {
+  var chart = diamondash.widgets.chart,
+      widgets = diamondash.widgets,
+      utils = diamondash.utils;
+
+  var PieDimensions = chart.views.ChartDimensions.extend({
+    defaults: _.extend({
+      scale: 0.6
+    }, chart.views.ChartDimensions.prototype.defaults),
+
+    scale: function() {
+      return this.get('scale');
+    },
+
+    height: function() {
+      return this.width() * this.scale();
+    },
+
+    radius: function() {
+      return (this.width() / 2) * this.scale();
+    },
+
+    offset: function() {
+      return {
+        x: this.width() / 2,
+        y: this.radius()
+      };
+    }
+  });
+
+  var PieView = chart.views.ChartView.extend({
+    id: function() {
+      return this.model.id;
+    },
+
+    margin: {
+      top: 4,
+      right: 4,
+      left: 4,
+      bottom: 0
+    },
+
+    bindings: {
+      'sync model': function() {
+        this.render();
+      }
+    },
+
+    initialize: function() {
+      PieView.__super__.initialize.call(this, {
+        dims: new PieDimensions({margin: this.margin})
+      });
+
+      this.arc = d3.svg.arc();
+
+      this.pie = d3.layout.pie().value(function(d) {
+        var datapoint = d.get('datapoints')[0];
+        return datapoint
+          ? datapoint.y
+          : 1;
+      });
+
+      utils.bindEvents(this.bindings, this);
+    },
+
+    render: function() {
+      this.dims.set({width: this.$el.width()});
+
+      this.arc
+        .outerRadius(this.dims.radius())
+        .innerRadius(0);
+
+      var g = this.canvas.selectAll('.arc')
+        .data(this.pie(this.model.get('metrics').models))
+        .enter().append('g')
+          .attr('class', 'arc');
+
+      g.append('path')
+        .attr('d', this.arc)
+        .style('fill', function(d) {
+          return d.data.get('color');
+        });
+
+      this.$el.append($(this.svg.node()));
+    }
+  });
+
+  widgets.registry.views.add('pie', PieView);
+
+  return {
+    PieView: PieView,
+    PieDimensions: PieDimensions
+  };
+}.call(this);
+
 diamondash.widgets.lvalue = function() {
   var widgets = diamondash.widgets,
       dynamic = diamondash.widgets.dynamic,
