d254990272c2ac12787c5875d3d7efda4615193e
justinvdm <justinvdm.0.1.0@gmail.com>
2014-04-23 11:37:13 +0200

    Set the pie widget's bucket size to its time range

--- diamondash/widgets/pie/pie.py
+++ diamondash/widgets/pie/pie.py
@@ -5,6 +5,11 @@ class PieWidgetConfig(ChartWidgetConfig):
     DEFAULTS = {}
     TYPE_NAME = 'pie'
 
+    @classmethod
+    def parse(cls, config):
+        config['bucket_size'] = config['time_range']
+        return super(PieWidgetConfig, cls).parse(config)
+
 
 class PieWidget(ChartWidget):
     CONFIG_CLS = PieWidgetConfig
