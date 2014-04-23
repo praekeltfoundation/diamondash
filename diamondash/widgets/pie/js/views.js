ab8523b2554a9b998121bf25923043b3451295ba
justinvdm <justinvdm.0.1.0@gmail.com>
2014-04-23 12:45:33 +0200

    Implement the basics for PieDimensions

--- diamondash/widgets/pie/js/views.js
+++ diamondash/widgets/pie/js/views.js
@@ -1,10 +1,32 @@
 diamondash.widgets.pie.views = function() {
-  var PieView = Backbone.View.extend({
+  var chart = diamondash.widgets.chart;
+
+  var PieDimensions = chart.views.ChartDimensions.extend({
+    defaults: _.extend({
+      scale: 0.6
+    }, chart.views.ChartDimensions.prototype.defaults),
+
+    scale: function() {
+      return this.get('scale');
+    },
+
+    height: function() {
+      return this.width() * this.scale();
+    },
+
+    radius: function() {
+      return (this.width() / 2) * this.scale();
+    },
+
+    offset: function() {
+      return {
+        x: this.width() / 2,
+        y: this.radius()
+      };
+    }
   });
 
-  widgets.registry.views.add('pie', PieView);
-
   return {
-    PieView: PieView
+    PieDimensions: PieDimensions
   };
 }.call(this);
--- diamondash/widgets/pie/tests/js/views.test.js
+++ diamondash/widgets/pie/tests/js/views.test.js
@@ -1,5 +1,44 @@
 describe("diamondash.widgets.pie", function() {
-  describe("PieView", function() {
+  var models = diamondash.widgets.pie.models,
+      views = diamondash.widgets.pie.views;
+
+  describe(".PieDimensions", function() {
+    var dims;
+
+    beforeEach(function() {
+      dims = new views.PieDimensions({
+        width: 64,
+        scale: 0.4,
+        margin: {
+          top: 2,
+          right: 2,
+          bottom: 2,
+          left: 2
+        }
+      });
+    });
+
+    it("should expose its scale", function() {
+        assert.equal(dims.scale(), 0.4);
+    });
+
+    it("should expose its height", function() {
+        assert.equal(dims.height(), 64 * 0.4);
+    });
+
+    it("should expose its radius", function() {
+        assert.equal(dims.radius(), (64 / 2) * 0.4);
+    });
+
+    it("should expose its offset", function() {
+        assert.deepEqual(dims.offset(), {
+            x: 64 / 2,
+            y: (64 / 2) * 0.4
+        });
+    });
+  });
+
+  describe(".PieView", function() {
     var pie;
 
     beforeEach(function() {
