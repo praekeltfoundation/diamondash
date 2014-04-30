b2247fd4b323142514c4f49aa4d838e400d4248e
justinvdm <justinvdm.0.1.0@gmail.com>
2014-04-30 15:42:29 +0200

    Boilerplate for histrogram widget view

--- diamondash/widgets/histogram/js/views.js
+++ diamondash/widgets/histogram/js/views.js
@@ -1,5 +1,19 @@
 diamondash.widgets.histogram.views = function() {
-  var HistogramView = Backbone.View.extend({
+  var chart = diamondash.widgets.chart,
+      widgets = diamondash.widgets;
+
+  var HistogramView = chart.views.XYChartView.extend({
+    height: 278,
+
+    initialize: function() {
+      HistogramView.__super__.initialize.call(this);
+    },
+
+    render: function() {
+      HistogramView.__super__.render.call(this);
+      this.$el.append($(this.svg.node()));
+      return this;
+    }
   });
 
   widgets.registry.views.add('histogram', HistogramView);
