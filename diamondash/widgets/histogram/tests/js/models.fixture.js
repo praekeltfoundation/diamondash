2624da1c85370f20414fdb7581cace9b138a815e
justinvdm <justinvdm.0.1.0@gmail.com>
2014-05-02 16:17:23 +0200

    Fix histogram model fixture to only contain a single metric (what the server-side gives us)

--- diamondash/widgets/histogram/tests/js/models.fixture.js
+++ diamondash/widgets/histogram/tests/js/models.fixture.js
@@ -12,15 +12,5 @@ diamondash.test.fixtures.add('diamondash.widgets.histogram.models.HistogramModel
       {x: 1340876895000, y: 16},
       {x: 1340877195000, y: 14},
       {x: 1340877495000, y: 24}]
-  }, {
-    id: 'metric-b',
-    name: 'metric-b',
-    datapoints: [
-      {x: 1340875995000, y: 8},
-      {x: 1340876295000, y: 22},
-      {x: 1340876595000, y: 2},
-      {x: 1340876895000, y: 8},
-      {x: 1340877195000, y: 12},
-      {x: 1340877495000, y: 16}]
   }]
 });
