747f635566e07022bef264f9d78f81da59883cc7
justinvdm <justinvdm.0.1.0@gmail.com>
2014-05-21 09:08:51 +0200

    Visual distinction between hovered over bars and the other bars for histogram widget

--- diamondash/widgets/histogram/js/views.js
+++ diamondash/widgets/histogram/js/views.js
@@ -1,19 +1,26 @@
 diamondash.widgets.histogram.views = function() {
-  var chart = diamondash.widgets.chart,
+  var utils = diamondash.utils,
+      chart = diamondash.widgets.chart,
       widgets = diamondash.widgets;
 
   var HistogramView = chart.views.XYChartView.extend({
     height: 278,
+    topMargin: 14,
     barPadding: 2,
-    format: {value: d3.format(".1s")},
+    format: {
+      short: d3.format(".2s"),
+      long: d3.format(",f")
+    },
 
     initialize: function() {
       HistogramView.__super__.initialize.call(this);
+      this.dims.set('margin', _.extend(this.dims.get('margin'), {
+        top: this.topMargin
+      }));
+      utils.bindEvents(this.bindings, this);
     },
 
-    render: function() {
-      HistogramView.__super__.render.call(this);
-
+    draw: function() {
       var self = this;
       var metric = this.model.get('metrics').at(0);
       var bucketSize = this.model.get('bucket_size');
@@ -47,26 +54,69 @@ diamondash.widgets.histogram.views = function() {
 
       rect
         .style('fill', metric.get('color'))
+        .style('pointer-events', 'none')
         .attr('width', barWidth)
         .attr('height', function(d) {
           return maxY - self.fy(d.y);
         });
 
-      var text = bar.selectAll('text').data(data, key);
+      var text = bar.selectAll('.value')
+        .data(data, key);
+
       text.enter().append('text')
         .attr('class', 'value')
-        .attr("text-anchor", "middle")
-        .attr("dy", ".75em")
-        .attr("y", 6);
+        .attr("dy", ".75em");
 
       text
-          .attr("x", barWidth / 2)
-          .text(function(d) {
-            return self.format.value(d.y);
-          });
+        .attr("x", barWidth / 2)
+        .attr("y", 6)
+        .text(function(d) {
+          return self.format.short(d.y);
+        });
 
+      this.svg.selectAll('.hover.bar .value')
+        .attr("y", -15)
+        .text(function(d) {
+          return self.format.long(d.y);
+        });
+    },
+
+    render: function() {
+      HistogramView.__super__.render.call(this);
+      this.draw();
       this.$el.append($(this.svg.node()));
       return this;
+    },
+
+    bindings: {
+      'hover': function(position) {
+        var bucketSize = this.model.get('bucket_size');
+
+        function match(d) {
+          return d.x === (position.x + bucketSize);
+        }
+
+        this.svg.selectAll('.bar')
+          .sort(function(d) {
+            return match(d)
+              ? 1
+              : 0;
+          })
+          .attr('class', function(d) {
+            return match(d)
+              ? 'hover bar'
+              : 'not-hover bar';
+          });
+
+        this.draw();
+      },
+
+      'unhover': function() {
+        this.svg.selectAll('.bar')
+          .attr('class', 'bar');
+
+        this.draw();
+      }
     }
   });
 
--- diamondash/widgets/histogram/tests/js/views.test.js
+++ diamondash/widgets/histogram/tests/js/views.test.js
@@ -20,7 +20,7 @@ describe("diamondash.widgets.histogram", function() {
         .domain(opts.model.range())
         .range([opts.chartHeight, 0]);
 
-      var format = d3.format(".1s");
+      var format = d3.format(".2s");
 
       var barWidth = fx(opts.model.get('bucket_size')) - fx(0);
       barWidth -= opts.barPadding;
@@ -73,6 +73,41 @@ describe("diamondash.widgets.histogram", function() {
 
     afterEach(function() {
       testUtils.unregisterModels();
+    });
+    
+    describe("when the chart is hovered over", function() {
+      it("should mark whether a bar is hovered", function() {
+        histogram.render();
+
+        testUtils.hover_axes(histogram, {
+          x: 1340876595000 - (5 * 1000 * 60)
+        });
+
+        var $bars = histogram.$('.bar');
+        assert.equal($bars.eq(0).attr('class'), 'not-hover bar');
+        assert.equal($bars.eq(1).attr('class'), 'hover bar');
+        assert.equal($bars.eq(2).attr('class'), 'not-hover bar');
+        assert.equal($bars.eq(3).attr('class'), 'not-hover bar');
+        assert.equal($bars.eq(4).attr('class'), 'not-hover bar');
+      });
+
+      it("should use a longer text format for the hovered over bar", function() {
+        histogram.model.get('metrics').at(0).set('datapoints', [
+          {x: 1340875995000, y: 18000000},
+          {x: 1340876295000, y: 12000000},
+          {x: 1340876595000, y: 6000000},
+          {x: 1340877195000, y: 21000000},
+          {x: 1340877495000, y: 24000000}]);
+
+        histogram.render();
+
+        testUtils.hover_axes(histogram, {
+          x: 1340876595000 - (5 * 1000 * 60)
+        });
+
+        var $bars = histogram.$('.bar');
+        assert.equal($bars.eq(1).text(), '6,000,000');
+      });
     });
 
     describe(".render", function() {
